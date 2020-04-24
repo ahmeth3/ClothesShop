25a82f5fce2ee805b6a28cfdae06c1379be34e5b
Ahmet Halilovic <akihalilovic@gmail.com>
2020-04-24 19:05:42 +0200

    odradje nacin placanja

--- src/app/components/support/nacini-placanja/nacini-placanja.component.ts
+++ src/app/components/support/nacini-placanja/nacini-placanja.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nacini-placanja',
   templateUrl: './nacini-placanja.component.html',
-  styleUrls: ['./nacini-placanja.component.css']
+  styleUrls: ['./nacini-placanja.component.css'],
 })
 export class NaciniPlacanjaComponent implements OnInit {
+  scrollActive = 1;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  changeScrollArrow(scroller) {
+    if (scroller === this.scrollActive) this.scrollActive = 0;
+    else this.scrollActive = scroller;
   }
-
 }

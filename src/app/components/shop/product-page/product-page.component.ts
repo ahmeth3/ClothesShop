600adb97dc1c5330e248f51950b63df17af5214a
Ahmet Halilovic <akihalilovic@gmail.com>
2020-05-06 21:37:15 +0200

    Sredjen dizajn sidebara
    Moram jos da napravim da ispod widtha od 992px sidebar bude collapse il nesto da vidim i da dodam filtere neke

--- src/app/components/shop/product-page/product-page.component.ts
+++ src/app/components/shop/product-page/product-page.component.ts
@@ -9,11 +9,21 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductPageComponent implements OnInit {
   category: string;
 
+  categoryActive: boolean = true;
+  colorsActive: boolean = true;
+  sizeActive: boolean = true;
+
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(
       (params) => (this.category = params.get('category'))
     );
+  }
+
+  scrollToggler(counter) {
+    if (counter === 1) this.categoryActive = !this.categoryActive;
+    if (counter === 2) this.colorsActive = !this.colorsActive;
+    if (counter === 3) this.sizeActive = !this.sizeActive;
   }
 }
